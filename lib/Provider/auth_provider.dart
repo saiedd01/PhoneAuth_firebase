5ee5ae6e27adbebf06a389bf4a4868ff0a2bb753
saiedd01 <sa3edahmed20@gmail.com>
2022-12-18 01:04:44 +0200

    Saied Ahmed- Create Function Sign with phone

--- lib/Provider/auth_provider.dart
+++ lib/Provider/auth_provider.dart
@@ -5,12 +5,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
+import 'package:phoneauth/Utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider extends ChangeNotifier{
 
   bool _isSignedIn = false;
   bool get isSignedIn => _isSignedIn;
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   AuthProvider(){
     CheckSignIn();
@@ -20,5 +23,22 @@ class AuthProvider extends ChangeNotifier{
     final SharedPreferences s = await SharedPreferences.getInstance();
     _isSignedIn = s.getBool("is_signedin") ?? false;
     notifyListeners();
+  }
+
+  void SignInWithPhone(BuildContext context, String phoneNumber) async {
+    try{
+      await _firebaseAuth.verifyPhoneNumber(
+          verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async{
+            await _firebaseAuth.signInWithCredential(phoneAuthCredential);
+          },
+          verificationFailed: (error){
+            throw Exception(error.message);
+          },
+          codeSent: codeSent,
+          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+    } on FirebaseAuthException
+    catch(e){
+      ShowSnackBar(context,e.message.toString());
+    }
   }
 }
